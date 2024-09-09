577d729bd8c62bb46b519041b3c6865b16dd8ef1
Wasif <111518026+Wasif-GFX@users.noreply.github.com>
2024-09-10 02:44:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,82 @@
+    // Assignment # 5
+
+
+// var sum = 5 + 8
+// document.write(sum)
+// var sub = 5 - 8
+// document.write(sub)
+// var mul = 5 * 8
+// document.write(mul)
+// var div = 5 / 8
+// document.write(div)
+
+var math
+math = 10;
+// document.write(math)
+
+inc = ++math
+// document.write(inc)
+
+add = math + 6
+// document.write(add)
+
+// dec = --math
+// document.write(dec)
+
+// var price
+// price = 600
+// document.write(price * 5)
+
+
+// var num = 4
+// document.write("<br/> 4 X 1 = " + num * 1)
+// document.write("<br/> 4 X 2 = " + num * 2)
+// document.write("<br/> 4 X 3 = " + num * 3)
+// document.write("<br/> 4 X 4 = " + num * 4)
+// document.write("<br/> 4 X 5 = " + num * 5)
+// document.write("<br/> 4 X 6 = " + num * 6)
+// document.write("<br/> 4 X 7 = " + num * 7)
+// document.write("<br/> 4 X 8 = " + num * 8)
+// document.write("<br/> 4 X 9 = " + num * 9)
+// document.write("<br/> 4 X 10 = " + num * 10)
+
+
+
+
+//     // Assignment # 6
+
+// var nums
+// nums = 3
+// // document.write(nums);
+// upd = ++nums
+// // document.write(upd);
+
+// upd = nums++
+// // document.write(upd);
+
+// upd = --nums
+// // document.write(upd);
+
+// upd = nums--
+// // document.write(upd);
+
+
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// // document.write(result)
+// document.write("a = 1")
+// document.write("b = 0")
+// document.write("Result = 3")
+
+
+// var fname = prompt("First Name")
+// var lname = prompt("Last Name")
+// var name = "My name is " + fname + " " + lname
+// document.write(name)
+
+
+// var table = +prompt("multiplication table of 5")
+// var mult = table*5
+// var answer = "5 X " + " "+ table + " " + "=" + " " + mult
+// document.write(answer)
+
